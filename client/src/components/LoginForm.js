f933c6cb7fe9c481b007acf77eda1888a7b79c0f
ericbarriga <ericbarriga@MacBook-Pro.local>
2022-04-19 11:55:25 -0600

    make it better

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -11,7 +11,7 @@ const LoginForm = () => {
   const [showAlert, setShowAlert] = useState(false);
 
   // initialize mutation
-  const [loginUser, { error }] = useMutation(LOGIN_USER);
+  const [loginUser,] = useMutation(LOGIN_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,4 +1,3 @@
-
 import { gql } from "@apollo/client";
 
 // mutation for logged in user
